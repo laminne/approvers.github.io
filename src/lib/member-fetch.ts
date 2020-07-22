c688657854f94ca12a33225b5afcf2396f12d936
MikuroXina <ryosukadnak@gmail.com>
2020-07-23 02:39:28 +0900

    Modify with zod

--- src/lib/member-fetch.ts
+++ src/lib/member-fetch.ts
@@ -1,60 +1,28 @@
-export type SNSLinkInfo = { type: "twitter"; url: string } | { type: "github"; url: string };
+import z from "zod";
 
-const validateSNSLink = (obj: unknown): obj is SNSLinkInfo => {
-  if (typeof obj !== "object" || obj == null) {
-    console.error("not object: ", obj);
-    return false;
-  }
+const SNSLinkInfoSchema = z.union([
+  z.object({
+    type: z.literal("twitter"),
+    url: z.string(),
+  }),
+  z.object({
+    type: z.literal("github"),
+    url: z.string(),
+  }),
+]);
 
-  if (!("type" in obj && ["twitter", "github"].includes((obj as SNSLinkInfo).type))) {
-    console.error("unknown type from: ", obj);
-    return false;
-  }
-  if (!("url" in obj && typeof (obj as SNSLinkInfo).url === "string")) {
-    console.error("`url` not in: ", obj);
-    return false;
-  }
-  return true;
-};
+export type SNSLinkInfo = z.infer<typeof SNSLinkInfoSchema>;
 
-export type Member = {
-  avatar: string;
-  name: string;
-  role: string;
-  links: SNSLinkInfo[];
-};
+const MemberSchema = z.object({
+  avatar: z.string(),
+  name: z.string(),
+  role: z.string(),
+  links: z.array(SNSLinkInfoSchema),
+});
 
-const validateMember = (obj: unknown): obj is Member => {
-  if (typeof obj !== "object" || obj == null) {
-    console.error("not object: ", obj);
-    return false;
-  }
+export type Member = z.infer<typeof MemberSchema>;
 
-  if (!("avatar" in obj && typeof (obj as Member).avatar === "string")) {
-    console.error("`avatar` not in: ", obj);
-    return false;
-  }
-  if (!("name" in obj && typeof (obj as Member).name === "string")) {
-    console.error("`name` not in: ", obj);
-    return false;
-  }
-  if (!("role" in obj && typeof (obj as Member).role === "string")) {
-    console.error("`role` not in: ", obj);
-    return false;
-  }
-  if (
-    !(
-      "links" in obj &&
-      typeof (obj as Member).links === "object" &&
-      (Object.values((obj as Member).links) as unknown[]).every(validateSNSLink)
-    )
-  ) {
-    console.error("`links` not in: ", obj);
-    return false;
-  }
-
-  return true;
-};
+const validateMember = MemberSchema.check;
 
 const validateMembers = (obj: unknown): obj is Member[] =>
   typeof obj === "object" && obj != null && (Object.values(obj) as unknown[]).every(validateMember);
